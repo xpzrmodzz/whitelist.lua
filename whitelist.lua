77e0dfbf894f3b2b6c8c7e4cee35f6bf75e181ad
xpzrmodzz <94053840+xpzrmodzz@users.noreply.github.com>
2024-03-31 21:45:03 +0200

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,11 +1,11 @@
 return {
-    [1724989894] = false, -- Remplacez 123456789 par l'ID de l'utilisateur
+    [1724989894] = true, -- Remplacez 123456789 par l'ID de l'utilisateur
     [2297130039] = true, -- Ajoutez autant d'entrées que nécessaire
     [720726589] = true,
     [2580390306] = true,-- Ajoutez d'autres IDs d'utilisateurs autorisés avec true
     [5464024414] = true,
-    [0] = true,
-    [0] = true,
+    [4806632507] = false,
+    [3821309722] = false,
     [5381341447] = true,
     [5382996039] = true,
     [668723420] = true,
