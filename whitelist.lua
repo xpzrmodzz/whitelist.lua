e18695c052187a37448c0b07accd1acd2c504c9f
xpzrmodzz <94053840+xpzrmodzz@users.noreply.github.com>
2024-04-22 11:33:30 +0200

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -22,6 +22,6 @@ return {
     [3436459659] = true,--said
     [3743512496] = true,--said
     [1046849011] = true,--said
-    [2652246414] = true,--clara
+    [2652246414] = false,--clara
     [783891553] = true, --Rkh
 }
